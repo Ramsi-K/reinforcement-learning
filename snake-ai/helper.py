fec60e55069a2bd235eda2e47063bce8a3e690e9
Ramsi Kalia <ramsi.kalia@gmail.com>
2024-02-12 19:34:13 +0530

    model training

--- snake-ai/helper.py
+++ snake-ai/helper.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from IPython import display
+import imageio
 
 plt.ion()
 
@@ -17,3 +18,24 @@ def plot(scores, mean_scores):
     plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
     plt.show(block=False)
     plt.pause(.1)
+
+def record_video(env, policy, out_directory, fps=30):
+  """
+  Generate a replay video of the agent
+  :param env
+  :param Qtable: Qtable of our agent
+  :param out_directory
+  :param fps: how many frame per seconds (with taxi-v3 and frozenlake-v1 we use 1)
+  """
+  images = []
+  done = False
+  state = env.reset()
+  img = env.render(mode='rgb_array')
+  images.append(img)
+  while not done:
+    # Take the action (index) that have the maximum expected future reward given that state
+    action, _ = policy.act(state)
+    state, reward, done, info = env.step(action) # We directly put next_state = state for recording logic
+    img = env.render(mode='rgb_array')
+    images.append(img)
+  imageio.mimsave(out_directory, [np.array(img) for i, img in enumerate(images)], fps=fps)
